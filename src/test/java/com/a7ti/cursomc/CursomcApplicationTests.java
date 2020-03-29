46c9768674ccb6f7da44041f18c09eec02db7283
Spartano007 <assermartins@hotmail.com>
2020-03-29 15:27:34 -0300

    Projeto criado

---
+++ src/test/java/com/a7ti/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a7ti.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
